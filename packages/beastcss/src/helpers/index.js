f3ec3bcb00c6bb98573e80582fff9690b13e984b
Freddy <freddyescobar@hotmail.fr>
2021-12-21 18:18:23 +0100

    refactor(beastcss): remove unused helpers index.js file

--- packages/beastcss/src/helpers/index.js
+++
@@ -1,5 +0,0 @@
-// export { replaceHTMLClasses, replaceCSSSelectors, restoreCSSSelectors } from './helpers/escapedChars';
-export * as escapedChars from './helpers/escapedChars';
-export { formatToKB, formatToMs, formatToPercent } from './helpers/formatter';
-export { defaultLogger, setVerbosity } from './helpers/log';
-export { readFile, writeFile, removeFile } from './helpers/promisify-fs';
