a204c286d30b2f1d5b633851b2e4cb94d87f35dc
Freddy <freddyescobar@hotmail.fr>
2022-08-15 15:11:58 +0200

    chore: remove config-lerna-scopes from commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,3 @@
-const { getPackages } = require('@commitlint/config-lerna-scopes').utils;
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': (ctx) =>
-      getPackages(ctx).then((packages) => [2, 'always', ['all', ...packages]]),
-  },
 };
